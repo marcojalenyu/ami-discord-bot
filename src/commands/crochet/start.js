b57260dffa24933c0f172b7f8b4a99a1484e6c87
Maracoo <marco_jalen_yu@dlsu.edu.ph>
2024-06-04 11:14:44 +0800

    Added /start for starting an existing pattern

---
+++ src/commands/crochet/start.js
@@ -0,0 +1,63 @@
+const { ApplicationCommandOptionType } = require("discord.js");
+const Basket = require("../../models/Basket");
+const Pattern = require("../../models/Pattern");
+
+module.exports = {
+    name: 'start',
+    description: 'Start crocheting a pattern.',
+    // deleted: true,
+    // devOnly: Boolean,
+    // testOnly: Boolean,
+    options: [
+        {
+            name: 'pattern',
+            description: 'The pattern to start.',
+            type: ApplicationCommandOptionType.String,
+            required: true
+        },
+    ],
+
+    callback: async (client, interaction) => {
+        try {
+            const basket = await Basket.findOne({ guildId: interaction.guildId }) || 
+                await Basket.findOne({ userId: interaction.user.id });
+            if (!basket || interaction.guildId !== basket.guildId) {
+                interaction.reply({
+                    content: 'Error: No basket registered. Please register a basket first.',
+                });
+                return;
+            } else {
+                // Find the pattern in the basket
+                const patternName = interaction.options.getString('pattern');
+                const pattern = await Pattern.findOne({ name: patternName, basketId: basket._id });
+                if (!pattern) {
+                    interaction.reply({ content: `Pattern "${patternName}" not found in basket.` });
+                    return;
+                } else {
+                    // Set the current pattern to the selected pattern
+                    basket.currentPattern = pattern._id;
+                    await basket.save();
+                    interaction.reply({ content: `Starting pattern "${patternName}".` });
+
+                    // Send the first step of the pattern (if it exists)
+                    if (pattern.steps.length > 0) {
+                        if (interaction.guildId) {
+                            interaction.channel.send(pattern.steps[0]);
+                        } else {
+                            interaction.user.send(pattern.steps[0]);
+                        }
+                    } else {
+                        if (interaction.guildId) {
+                            interaction.channel.send('No steps recorded yet. Start recording steps with `/record`');
+                        } else {
+                            interaction.user.send('No steps recorded yet. Start recording steps with `/record`');
+                        }
+                    }
+                }
+            }
+        } catch (error) {
+            console.error(error);
+            interaction.reply('Oh no! The instructions got crumpled! Please try again.');
+        }
+    }
+}
