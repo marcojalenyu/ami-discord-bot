96abca96b0109aa5abea68d6d072cb5c4f7fcc90
Maracoo <marco_jalen_yu@dlsu.edu.ph>
2024-06-04 11:15:16 +0800

    Added a way to handle messages for saving steps

---
+++ src/events/messageCreate/handleMessages.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const devs = process.env.DEVS.split(',');
+const testServer = process.env.TEST_SERVER;
+const Basket = require('../../models/Basket');
+const Pattern = require('../../models/Pattern');
+
+module.exports = async (client, message) => {
+    try {
+        if (message.author.bot) return;
+        // Check if the guild or user is in the database
+        const basket = await Basket.findOne({ guildId: message.guild.id }) ||
+            await Basket.findOne({ userId: message.author.id });
+
+        // If the basket is not found or the guild does not match the basket's guild
+        if (!basket || message.guild.id !== basket.guildId) {
+            return;
+        } else {
+            console.log(basket.channelId, message.channel.id)
+            if (basket.isRecording && basket.channelId === message.channel.id) {
+                const steps = message.content.split('\n');
+                const currentPattern = await Pattern.findById(basket.currentPattern);
+                
+                currentPattern.steps.push(...steps);
+                await currentPattern.save();
+            }
+        }
+    } catch (error) {
+        console.error("There was an error handling the command.");
+    }
+};
