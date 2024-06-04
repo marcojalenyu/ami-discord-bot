cc5d421b21452ce3c9b115275fa04fbb03e9cee5
Maracoo <marco_jalen_yu@dlsu.edu.ph>
2024-06-04 13:59:59 +0800

    Added /steps to allow viewing all steps in a pattern

---
+++ src/commands/crochet/steps.js
@@ -0,0 +1,52 @@
+const { ApplicationCommandOptionType } = require("discord.js");
+const Pattern = require("../../models/Pattern");
+const Basket = require("../../models/Basket");
+
+module.exports = {
+    name: 'steps',
+    description: 'View steps in a pattern.',
+    // deleted: true,
+    // devOnly: Boolean,
+    // testOnly: Boolean,
+    options: [
+        {
+            name: 'name',
+            description: 'Label your pattern',
+            type: ApplicationCommandOptionType.String,
+            required: true
+        },
+    ],
+
+    callback: async (client, interaction) => {
+        try {
+            const basket = await Basket.findOne({ guildId: interaction.guildId }) ||
+                await Basket.findOne({ userId: interaction.user.id });
+
+            if (!basket || interaction.guildId !== basket.guildId) {
+                interaction.reply({
+                    content: 'Error: No basket registered. Please register a basket first.',
+                });
+                return;
+            } else {
+                const name = interaction.options.getString('name');
+                const pattern = await Pattern.findOne({ name: name, basketId: basket._id });
+
+                if (!pattern) {
+                    interaction.reply({ content: `Pattern "${name}" not found in basket.` });
+                    return;
+                } else {
+                    const steps = pattern.steps;
+                    if (steps.length === 0) {
+                        interaction.reply({ content: `No steps found in pattern "${name}".` });
+                        return;
+                    } else {
+                        interaction.reply({ content: `Steps in pattern "${name}":\n${steps.join('\n')}` });
+                    }
+                }
+            }
+        } catch (error) {
+            console.error(error);
+            interaction.reply('Oh no! The pattern got crumpled! Please try again.');
+        }
+    }
+}
