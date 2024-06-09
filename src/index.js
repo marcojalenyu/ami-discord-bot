c094e0ec4f7911cbe4522228a004786a3d4e1da1
Maracoo <marco_jalen_yu@dlsu.edu.ph>
2024-06-09 23:47:14 +0800

    Updated index.js in the hopes it will keep bot alive

--- src/index.js
+++ src/index.js
@@ -18,6 +18,14 @@ const client = new Client({
         mongoose.connect(process.env.MONGODB_URI);
         console.log('Connected to MongoDB');
         eventHandler(client);
+        
+        // Check the current time every minute to help keep the bot alive
+        setInterval(async () => {
+            const now = new Date();
+            const currentTime = `${now.getHours()}:${now.getMinutes()}`;
+            console.log(`Current time: ${currentTime}`);
+        }, 60 * 1000);
+
     } catch (error) {
         console.error('Error connecting to MongoDB');
     }
