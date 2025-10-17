0b15b7650a632b3d5beff00d92669e0656acb320
marcojalenyu <112309140+marcojalenyu@users.noreply.github.com>
2025-10-17 19:20:11 +0800

    feat: Changed to decimal, redid removing tax version

--- src/commands/crochet/removetax.js
+++ src/commands/crochet/removetax.js
@@ -1,15 +1,15 @@
 const { ApplicationCommandOptionType, MessageFlags } = require("discord.js");
 
 module.exports = {
-    name: 'addtax',
-    description: 'Compute price of product with tax (i.e. PayPal fee)',
+    name: 'removetax',
+    description: 'Compute tax amount of product and its price pre-tax (i.e. PayPal fee)',
     // deleted: true,
     // devOnly: Boolean,
     // testOnly: Boolean,
     options: [
         {
             name: 'price',
-            description: 'Raw price of the product',
+            description: 'Total price of the product',
             type: ApplicationCommandOptionType.Number,
             required: true,
         },
@@ -29,44 +29,41 @@ module.exports = {
 
     callback: async (client, interaction) => {
         try {
-            const price = interaction.options.getNumber('price');
+            const totalPrice = interaction.options.getNumber('price');
             const taxRate = interaction.options.getNumber('tax');
             const percentFee = interaction.options.getNumber('fee') || 0;
 
-            const tax = price * taxRate;
-            const totalPrice = price + tax;
-            const fee = totalPrice * percentFee;
+            // total = P * (1 + t) * (1 + f)
+            const priceBeforeTax = totalPrice / (1+taxRate);
+            const taxAmount = priceBeforeTax * taxRate;
+            const feeAmount = totalPrice * percentFee;
+            const priceBeforeTaxAndFee = priceBeforeTax - feeAmount;
 
             let message = `Price Breakdown\n`;
             message += `-----------\n`;
-            message += `Raw Price (P): ${price}\n`;
-            message += `Tax Rate (t): ${(taxRate*100).toFixed(2)}%\n`;
+            message += `Total Price (incl. tax & fee): ${totalPrice.toFixed(2)}\n`;
+            message += `Tax Rate (t): ${(taxRate * 100).toFixed(2)}%\n`;
             if (percentFee > 0) {
-                message += `Fee Rate (f): ${(percentFee*100).toFixed(2)}%\n`;
+                message += `Fee Rate (f): ${(percentFee * 100).toFixed(2)}%\n`;
             }
             message += `-----------\n`;
-            message += `Raw Price (P): ${price}\n`;
-            message += `Tax Amount (T = P x t): ${tax.toFixed(2)}\n`;
+            message += `Raw Price (P): ${priceBeforeTax.toFixed(2)}\n`;
+            message += `Tax Amount (T = P x t): ${taxAmount.toFixed(2)}\n`;
             if (percentFee > 0) {
-                message += `Fee (F = (P + T) x f): ${fee.toFixed(2)}\n`;
+                message += `Fee (F = (P + T) x f): ${feeAmount.toFixed(2)}\n`;
             }
             message += `-----------\n`;
-            message += `Price: ${(price-fee).toFixed(2)}\n`;
-            message += `Tax: ${tax.toFixed(2)}\n`;
-            if (percentFee > 0) {
-                message += `Fee: ${fee.toFixed(2)}\n`;
-            }
-            message += `-----------\n`;
-            message += `Subtotal: ${(totalPrice).toFixed(2)}\n`;
+            message += `Raw Price (excluding fees): ${priceBeforeTaxAndFee.toFixed(2)}\n`;
+
             interaction.reply({
                 content: message,
-                MessageFlags: MessageFlags.Ephemeral
+                flags: MessageFlags.Ephemeral
             });
         } catch (error) {
             console.error(error);
-            interaction.reply({ 
-                content: 'Oh no! The calculator ran out of batteries. Try again.', 
-                MessageFlags: MessageFlags.Ephemeral
+            interaction.reply({
+                content: 'Oh no! The calculator ran out of batteries. Try again.',
+                flags: MessageFlags.Ephemeral
             });
         }
     }
